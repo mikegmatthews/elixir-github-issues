329bc120fc55209ba7f96f8798706fe60a7fa0af
Mike Matthews <michael.g.matthews5@gmail.com>
2022-10-04 22:06:54 -0400

    Added dependencies

--- mix.exs
+++ mix.exs
@@ -21,8 +21,16 @@ defmodule Issues.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      { :httpoison, "~> 1.8.2" },
+      { :poison, "~> 5.0.0" },
+      { :ex_doc, "~> 0.28.5"},
+      { :earmark, "~> 1.4"},
+    ]
+  end
+
+  defp escript_config do
+    [
+      main_module: Issues.CLI
     ]
   end
 end
